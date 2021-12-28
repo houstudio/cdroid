8286497617e743cfb6240050fd8bfef66eb2d956
侯歌 <hou731080@163.com>
2021-12-28 16:58:01 +0800

    fix optional_libs for plplot

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -12,7 +12,7 @@ option(ENABLE_GESTURE "enable gestrure" OFF)
 option(ENABLE_TURBOJPEG "enable turbo jpeg" ON)
 option(ENABLE_MP3ID3 "enable mp3 ids" ON)
 option(ENABLE_FRIBIDI "BIDI Text Layout  support" ON)
-option(ENABLE_PLPLOT "Enable PLPLot" OFF)
+option(ENABLE_PLPLOT "Enable PLPLot" ON)
 option(WITH_JPEG8 "Emulate libjpeg v8 API/ABI (this makes ${CMAKE_PROJECT_NAME} backward-incompatible with libjpeg v6b)" ON)
 option(FT_WITH_HARFBUZZ "Improve auto-hinting of OpenType fonts." OFF)
 
@@ -65,7 +65,7 @@ if(ENABLE_FRIBIDI)
 endif()
 
 if(ENABLE_PLPLOT)
-  list(APPEND plplot plplotcxx)
+  list(APPEND OPTIONAL_LIBS plplot plplotcxx)
 endif()
 
 if(ENABLE_PINYIN2HZ)
