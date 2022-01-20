9134e6e3f69cc062ca298050f81bdd37d2526da8
侯歌 <hou731080@163.com>
2022-01-20 16:44:21 +0800

    add transparent to color parser

--- src/gui/core/color.cc
+++ src/gui/core/color.cc
@@ -2,10 +2,10 @@
 #include <map>
 namespace cdroid{
 static std::map<const std::string ,unsigned int>sColorNameMap={
-    {"aquamarine", 0x7FFFD4},    {"beige", 0xF5F5DC},       {"black", 0x000000},         {"blue", 0x0000FF},
+    {"aquamarine",0x7FFFD4},    {"beige", 0xF5F5DC},       {"black", 0x000000},         {"blue", 0x0000FF},
     {"blueviolet",0x8A2BE2},     {"brown", 0xA52A2A},       {"cadetblue", 0x5F9EA0},     {"chartreuse", 0x7FFF00},
-    {"chocolate", 0xD2691E},     {"coral", 0xFF7F50},       {"cornflowerblue", 0x6495ED},{"cyan", 0x00FFFF},
-    {"firebrick", 0xB22222},     {"forestgreen", 0x228B22}, {"gold", 0xFFD700},          {"goldenrod", 0xDAA520},
+    {"chocolate" ,0xD2691E},     {"coral", 0xFF7F50},       {"cornflowerblue", 0x6495ED},{"cyan", 0x00FFFF},
+    {"firebrick" ,0xB22222},     {"forestgreen", 0x228B22}, {"gold", 0xFFD700},          {"goldenrod", 0xDAA520},
     {"gray", 0xBEBEBE},          {"green", 0x00FF00},       {"greenyellow", 0xADFF2F},   {"hotpink", 0xFF69B4},
     {"indianred", 0xCD5C5C},     {"khaki", 0xF0E68C},       {"lawngreen", 0x7CFC00},     {"limegreen", 0x32CD32},
     {"magenta", 0xFF00FF},       {"maroon", 0xB03060},      {"navy", 0x000080},          {"orange", 0xFFA500},
@@ -63,7 +63,9 @@ unsigned int Color::parseColor(const std::string& colorString){
         if(colorString.length()<=7)
             cc|=0xFF000000;
         return cc;
-    }else{
+    }else if(colorString.compare("transparent")==0){
+        return 0;
+    }else
         return 0xFF000000|getHtmlColor(colorString);
     }
 }
