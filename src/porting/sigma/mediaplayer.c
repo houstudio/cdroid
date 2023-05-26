6fecb463b2dfd3cb0b224994ef12bff8fe4d7d04
houzh <houstudio@github.com>
2023-05-26 08:48:38 +0000

    modify mediaplayer

--- src/porting/sigma/mediaplayer.c
+++ src/porting/sigma/mediaplayer.c
@@ -6,46 +6,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <interface.h>
-
+#include <cdgraph.h>
 
 typedef struct{
    void*hplayer;
+   char*mediaURL;
    MP_CALLBACK cbk;
    void*userdata;
-}NGL_PLAYER;
+   GFXRect viewPort;
+}MP_PLAYER;
 
 HANDLE MPOpen(const char*fname){
-    NGL_PLAYER *mp=(NGL_PLAYER*)malloc(sizeof(NGL_PLAYER));
-    memset(mp,0,sizeof(NGL_PLAYER));
-    char*p=strchr(fname,':');
-    mm_player_open(fname,0,0,0,0);// uint16_t x, uint16_t y, uint16_t width, uint16_t height)
+    MP_PLAYER *mp=(MP_PLAYER*)malloc(sizeof(MP_PLAYER));
+    memset(mp,0,sizeof(MP_PLAYER));
+    mp->mediaURL=strdup(fname);//
     return mp;
 }
 
 DWORD MPPlay(HANDLE handle){
-    NGL_PLAYER*mp=(NGL_PLAYER*)handle;
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
+    GFXRect r = mp->viewPort;
+    mm_player_open(mp->mediaURL,r.x,r.y,r.w,r.h);// uint16_t x, uint16_t y, uint16_t width, uint16_t height)
     return E_OK;
 }
 
 DWORD MPStop(HANDLE handle){
-    NGL_PLAYER*mp=(NGL_PLAYER*)handle;
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
+    //mm_player_stop();no stop api
     return E_OK;
 }
 
 DWORD nglMPResume(HANDLE handle){
-    NGL_PLAYER*mp=(NGL_PLAYER*)handle;
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
     mm_player_resume();
     return E_OK;
 }
 
 DWORD MPPause(HANDLE handle){
-    NGL_PLAYER*mp=(NGL_PLAYER*)handle;
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
     mm_player_pause();
     return E_OK;
 }
 
 DWORD MPClose(HANDLE handle){
-    NGL_PLAYER*mp=(NGL_PLAYER*)handle;
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
+    free(mp->mediaURL);
+    free(mp);
     mm_player_close();
     return E_OK;
 }
@@ -60,14 +66,23 @@ DWORD MPGetDuration(HANDLE handle,double*dur){
 }
 
 DWORD MPSeek(HANDLE handle,double pos){
-    NGL_PLAYER*mp=(NGL_PLAYER*)handle;
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
     //mpg_cmd_set_speed
     mm_player_seek2time(pos);
 }
 
-DWORD SetCallback(HANDLE handle,MP_CALLBACK cbk,void*userdata){
-    NGL_PLAYER*mp=(NGL_PLAYER*)handle;
+DWORD MPSetCallback(HANDLE handle,MP_CALLBACK cbk,void*userdata){
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
     mp->cbk=cbk;
     mp->userdata=userdata;
 }
 
+DWORD MPSetWindow(HANDLE handle,int x,int y,int width,int height){
+    MP_PLAYER*mp=(MP_PLAYER*)handle;
+    GFXRect *r=&mp->viewPort;
+    r->x = x;
+    r->y = y;
+    r->w = width;
+    r->h = height;
+    return 0;
+}
