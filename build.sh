bfc0681b6f3c0126e6e5782f577b6926cde8069c
houzh <houzh@msn.com>
2023-08-03 18:24:50 +0800

    add r818 cmake files

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ declare -A TOOLCHAINS #key/value dict ,key is platform,value is toolchain,key mu
 TOOLCHAINS["SIGMA"]=$HOME/cdroid/cmake/ssd202-mtitoolchain.cmake
 TOOLCHAINS["ALI3528"]=$HOME/cdroid/cmake/ali3528-mtitoolchain.cmake
 TOOLCHAINS["RISCVD211"]=$HOME/cdroid/cmake/riscv64-d211-toolchain.cmake
-
+TOOLCHAINS["R818"]=$HOME/cdroid/cmake/aarch64-toolchain.cmake
 declare -A DEPLIBS #key/value dict,key is platform,value is deplibs dir in vcpkg,key must be uppercase
 DEPLIBS["X64"]=$HOME/vcpkg/installed/x64-linux-dynamic
 DEPLIBS["SIGMA"]=$HOME/vcpkg/installed/arm-linux-dynamic
