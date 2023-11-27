a2246eb543759209b1b73cacacca02f07294623d
houzh <houzh@msn.com>
2023-11-27 14:52:22 +0800

    fix textview's textcolor issue

--- src/gui/widget/textview.cc
+++ src/gui/widget/textview.cc
@@ -1584,7 +1584,7 @@ void TextView::updateTextColors(){
     bool inval = false;
     int color;
     const std::vector<int>&drawableState = getDrawableState();
-    if (mCurTextColor) {
+    if (mTextColor) {
         color = mTextColor->getColorForState(drawableState,0);
         LOGV("%p:%d change color %x->%x",this,mID,color,mCurTextColor);
         mCurTextColor = color;
