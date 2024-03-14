b89032af027402b6dcd02498a0eacf2c8d70118b
houzh <houzh@msn.com>
2024-03-14 12:00:39 +0800

    add bitmapdrawable.setAntiAlias

--- src/gui/drawables/animatedrotatedrawable.cc
+++ src/gui/drawables/animatedrotatedrawable.cc
@@ -12,7 +12,6 @@ AnimatedRotateDrawable::AnimatedRotateState::AnimatedRotateState(){
     mPivotXRel=mPivotYRel=false;
     mFrameDuration=150;
     mFramesCount=12;
-    mAntiAlias = false;
 }
 
 AnimatedRotateDrawable::AnimatedRotateState::AnimatedRotateState(const AnimatedRotateState& orig)
@@ -23,7 +22,6 @@ AnimatedRotateDrawable::AnimatedRotateState::AnimatedRotateState(const AnimatedR
     mPivotY = orig.mPivotY;
     mFramesCount = orig.mFramesCount;
     mFrameDuration = orig.mFrameDuration;
-    mAntiAlias = orig.mAntiAlias;
 }
 
 AnimatedRotateDrawable* AnimatedRotateDrawable::AnimatedRotateState::newDrawable(){
