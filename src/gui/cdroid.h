12d6c572370d75c41ed477d70545fadb139c2c24
侯歌 <hou731080@163.com>
2024-07-30 18:53:16 +0800

    remove build.h from cdroid.h

--- src/gui/cdroid.h
+++ src/gui/cdroid.h
@@ -16,7 +16,7 @@
  * 02110-1301, USA.
  */
 #include <core/app.h>
-#include <core/build.h>
+//#include <core/build.h>
 #include <gui/gui_features.h>
 #include <view/viewgroup.h>
 #include <view/viewoverlay.h>
