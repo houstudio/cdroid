7dfd8f95322632db5027af09b807197d964e5dee
侯歌 <hou731080@163.com>
2024-09-18 16:19:01 +0800

    fix imagetest

--- src/gui/image-decoders/image-test.cc
+++ src/gui/image-decoders/image-test.cc
@@ -12,7 +12,8 @@ int main(int argc,const char*argv[]){
     auto image=ImageDecoder::loadImage(nullptr,argv[1]);
     image->write_to_png("111.png");
 #else
-    cdroid::FrameSequence*seq=new cdroid::PngFrameSequence(nullptr,argv[1]);
+    std::ifstream file(argv[1]);
+    cdroid::FrameSequence*seq=new cdroid::PngFrameSequence(file);
     LOGD("%dx%dx%d",seq->getWidth(),seq->getHeight(),seq->getFrameCount());
     Cairo::RefPtr<Cairo::ImageSurface>img=Cairo::ImageSurface::create(Cairo::Surface::Format::ARGB32,seq->getWidth(),seq->getHeight());
     cdroid::FrameSequenceState*state=seq->createState();
