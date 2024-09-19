05467c771e5134564b1e64ebcb328f5583e4c2f3
侯歌 <hou731080@163.com>
2024-09-19 22:49:02 +0800

    fix InputEventSource::needCancel

--- src/gui/core/inputeventsource.cc
+++ src/gui/core/inputeventsource.cc
@@ -109,7 +109,7 @@ bool InputEventSource::needCancel(InputDevice*dev){
     nsecs_t etime;
     const nsecs_t now = SystemClock::uptimeMillis();
     dev->getLastEvent(action,etime);
-    if( (action==MotionEvent::ACTION_MOVE) && (now - etime>300) ){
+    if( (action!=MotionEvent::ACTION_CANCEL) && (now - etime>300) ){
         MotionEvent*e = MotionEvent::obtain(now, now, MotionEvent::ACTION_CANCEL, 0, 0, 0);
         dev->pushEvent(e);
     }
