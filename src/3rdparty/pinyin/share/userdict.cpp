8ae33cc2dd501e8218153054ba5103be1fba606a
zhhou <13923402541@qq.com>
2024-10-15 17:44:10 +0800

    modify pinyin.userdict

--- src/3rdparty/pinyin/share/userdict.cpp
+++ src/3rdparty/pinyin/share/userdict.cpp
@@ -32,6 +32,9 @@
 #define open   _open
 #define read  _read
 #define write _write
+#define seek _seek
+#define unlink _unlink
+#define strdup _strdup
 void gettimeofday(struct timeval* tv, struct timezone* local) {
     ULONGLONG tick = GetTickCount64();
     if (tv) {
