ed0cf8ffee50138682c7b0104bc21b0455ab3e15
houzh <hou731080@163.com>
2024-10-25 19:55:40 +0800

    tinat113 g2d added and compile passed

--- src/porting/tinat113/graph_g2d.c
+++ src/porting/tinat113/graph_g2d.c
@@ -11,8 +11,9 @@
 #include <linux/input.h>
 #include <cdinput.h>
 #include <g2d_driver.h>
-#include <libhardware2/fb.h>
 #include <pixman.h>
+
+#define USE_PIXMAN 1
 
 typedef struct {
     int fb;
@@ -71,11 +72,10 @@ int32_t GFXInit() {
     const size_t displayScreenSize=(dev->var.yres * dev->fix.line_length);
     const size_t screenSize = (dev->var.yres - screenMargin.y - screenMargin.h) * (dev->fix.line_length - (screenMargin.x + screenMargin.w)*4);
     const size_t numSurface=(dev->fix.line_length-displayScreenSize)/screenSize+1;
-    char*fbp = (char *)mmap(0,dev->fix.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, dev->fb, 0);
+    char*buffStart = (char *)mmap(0,dev->fix.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, dev->fb, 0);
     char*kbuffStart = (const char*)devs[0].fix.smem_start;
-    char*buffStart = fbp;
-    devSurfaces[0].kbuffer= buffStart;
-    devSurfaces[0].buffer = fbp;
+    devSurfaces[0].kbuffer= kbuffStart;
+    devSurfaces[0].buffer = buffStart;
     devSurfaces[0].width  = dev->var.xres;
     devSurfaces[0].height = dev->var.yres;
     devSurfaces[0].pitch  = dev->fix.line_length;
@@ -91,7 +91,7 @@ int32_t GFXInit() {
         buffStart+=screenSize;
     }
     dev->var.yoffset=0;//set first screen memory for display
-    LOGI("FBIOPUT_VSCREENINFO=%d",ioctl(dev->fb,FBIOPUT_VSCREENINFO,&dev->var));
+    LOGI("FBIOPUT_VSCREENINFO=%d g2dfd=%d",ioctl(dev->fb,FBIOPUT_VSCREENINFO,&dev->var),dev->g2d);
     LOGI("fb solution=%dx%d accel_flags=0x%x\r\n",dev->var.xres,dev->var.yres,dev->var.accel_flags);
     return E_OK;
 }
@@ -135,21 +135,44 @@ int32_t GFXSurfaceSetOpacity(GFXHANDLE surface,uint8_t alpha) {
 
 int32_t GFXFillRect(GFXHANDLE surface,const GFXRect*rect,uint32_t color) {
     FBSURFACE*ngs=(FBSURFACE*)surface;
+    FBDEVICE*dev =&devs[ngs->dispid];
     uint32_t x,y;
     GFXRect rec= {0,0,0,0};
     rec.w=ngs->width;
     rec.h=ngs->height;
     if(rect)rec=*rect;
     LOGV("FillRect %p %d,%d-%d,%d color=0x%x pitch=%d",ngs,rec.x,rec.y,rec.w,rec.h,color,ngs->pitch);
-    uint32_t*fb=(uint32_t*)(ngs->buffer+ngs->pitch*rec.y+rec.x*4);
-    uint32_t*fbtop=fb;
-    for(x=0; x<rec.w; x++)fb[x]=color;
-    const int cpw=rec.w*4;
-    long copied=0;
-    for(y=1; y<rec.h; y++) {
-        fb+=(ngs->pitch>>2);
-        memcpy(fb,fbtop,cpw);
-        copied+=ngs->pitch;
+    if(ngs->kbuffer==0){
+        uint32_t*fb=(uint32_t*)(ngs->buffer+ngs->pitch*rec.y+rec.x*4);
+        uint32_t*fbtop=fb;
+        for(x=0; x<rec.w; x++)fb[x]=color;
+        const int cpw=rec.w*4;
+        long copied=0;
+        for(y=1; y<rec.h; y++) {
+            fb+=(ngs->pitch>>2);
+	    memcpy(fb,fbtop,cpw);
+	    copied+=ngs->pitch;
+        }
+    }else{
+        g2d_fillrect_h fill;
+	bzero(&fill,sizeof(fill));
+        fill.dst_image_h.mode = G2D_PIXEL_ALPHA;
+        fill.dst_image_h.color = color;
+        fill.dst_image_h.format = G2D_FORMAT_RGB888;
+        fill.dst_image_h.clip_rect.x = rec.x;
+        fill.dst_image_h.clip_rect.y = rec.y;
+        fill.dst_image_h.clip_rect.w = rec.w;
+        fill.dst_image_h.clip_rect.h = rec.h;
+        fill.dst_image_h.width = ngs->width;
+        fill.dst_image_h.height = ngs->height;
+        fill.dst_image_h.align[0] = 0;
+        fill.dst_image_h.align[1] = fill.dst_image_h.align[0];
+        fill.dst_image_h.align[2] = fill.dst_image_h.align[0];
+        fill.dst_image_h.laddr[0] = (uintptr_t) ngs->kbuffer;//sunxifb_mem_get_phyaddr(dest_buf);
+        fill.dst_image_h.laddr[1] = (uintptr_t) 0;
+        fill.dst_image_h.laddr[2] = (uintptr_t) 0;
+        fill.dst_image_h.use_phy_addr = 1;
+	ioctl(dev->g2d, G2D_CMD_FILLRECT_H, (uintptr_t)(&fill));
     }
     return E_OK;
 }
@@ -215,7 +238,7 @@ static FBSURFACE*getFreeSurface(){
     return NULL;
 }
 
-int32_t GFXCreateSurface(int dispid,GFXHANDLE*surface,uint32_t width,uint32_t height,int32_t format,BOOL hwsurface) {
+int32_t GFXCreateSurface(int dispid,GFXHANDLE*surface,uint32_t width,uint32_t height,int32_t format,bool hwsurface) {
     FBSURFACE*surf=getFreeSurface();
     FBDEVICE*dev = &devs[dispid];
     surf->dispid=dispid;
@@ -280,46 +303,46 @@ int32_t GFXBlit(GFXHANDLE dstsurface,int dx,int dy,GFXHANDLE srcsurface,const GF
     else pbd+=(dy+screenMargin.y)*ndst->pitch+(dx+screenMargin.x)*4;
     const int cpw=rs.w*4;
     if(dev->g2d<0){
-#if 0
+#ifndef USE_PIXMAN
         for(y=0; y<rs.h; y++) {
             memcpy(pbd,pbs,cpw);
             pbs+=nsrc->pitch;
             pbd+=ndst->pitch;
         }
 #else
-    pixman_image_t *src_image = pixman_image_create_bits(PIXMAN_a8r8g8b8, nsrc->width, nsrc->height, nsrc->buffer, nsrc->pitch);
-    pixman_image_t *dst_image = pixman_image_create_bits(PIXMAN_a8r8g8b8, ndst->width, ndst->height, ndst->buffer, ndst->pitch);
-    pixman_image_composite(PIXMAN_OP_SRC, src_image,NULL/*mask*/, dst_image,
+        pixman_image_t *src_image = pixman_image_create_bits(PIXMAN_a8r8g8b8, nsrc->width, nsrc->height, nsrc->buffer, nsrc->pitch);
+        pixman_image_t *dst_image = pixman_image_create_bits(PIXMAN_a8r8g8b8, ndst->width, ndst->height, ndst->buffer, ndst->pitch);
+        pixman_image_composite(PIXMAN_OP_SRC, src_image,NULL/*mask*/, dst_image,
                        rs.x, rs.y, 0, 0, dx, dy, rs.w, rs.h);
-    pixman_image_unref(src_image);
-    pixman_image_unref(dst_image);	
+        pixman_image_unref(src_image);
+        pixman_image_unref(dst_image);	
 #endif
     }else{
-        g2d_blit_h blt;
+        g2d_blt_h blt;
 	bzero(&blt,sizeof(blt));
-	blt.flag_h = G2D_BIT_COPYPEN;
-        blt.src.image_h.width = nsrc.>width;
-        blt.src.image_h.height = nsrc->height;
-        blt.src.image_h.laddr[0]=(uintptr t)(nsrc->kbuffer?nsrc->kbuffer:nsrc->buffer);
-        blt.src .mage_h.clip_rect.x = rs.x;
-        blt.src.image_h.clip_rect,y = rs.y;
-        blt.src.image_h.clip_rect.w= rs.w;
-        blt.src.image_h.clip_rect.h = rs.h;
-        blt.src.image_h.format =G2D_FORMAT_RGB888;
-        blt.src.image_h.alpha = 255;
-        blt.src.image_h.use phy_addr = (nsrc->kbuffer!=NULL);
+	blt.flag_h = G2D_BLT_NONE;
+        blt.src_image_h.width = nsrc->width;
+        blt.src_image_h.height = nsrc->height;
+        blt.src_image_h.laddr[0]=(uintptr_t)(nsrc->kbuffer?nsrc->kbuffer:nsrc->buffer);
+        blt.src_image_h.clip_rect.x = rs.x;
+        blt.src_image_h.clip_rect,y = rs.y;
+        blt.src_image_h.clip_rect.w= rs.w;
+        blt.src_image_h.clip_rect.h = rs.h;
+        blt.src_image_h.format =G2D_FORMAT_RGB888;
+        blt.src_image_h.alpha = 255;
+        blt.src_image_h.use_phy_addr = (nsrc->kbuffer!=NULL);
         //blit dest info
-        blt.dst.image_h.clip_rect.x = dx;
-        blt.dst.image_h.clip_rect.y = dy;
-        blt.dst.image_h.clip_rect.w= rs.w;
-        blt.dst.image_h.clip_rect.h = rs.h;
-        blt.dst.image_h.width = ndst.>width;
-        blt.dst.image h.height = ndst->height;
-        blt.dst.image_h.laddr[0]=(uintptr t)(ndst->kbuffer?ndst->kbuffer:ndst->buffer);
-        blt.dst.image_h.format =G2D_FORMAT_RGB888;
-        blt.dst.image_h.alpha = 255;
-        blt.dst.image_h.use_phy_addr = (ndst->kbuffer!=NULL);
-        blt.dst.image_h.color = 0xee8899;
+        blt.dst_image_h.clip_rect.x = dx;
+        blt.dst_image_h.clip_rect.y = dy;
+        blt.dst_image_h.clip_rect.w= rs.w;
+        blt.dst_image_h.clip_rect.h = rs.h;
+        blt.dst_image_h.width = ndst->width;
+        blt.dst_image_h.height = ndst->height;
+        blt.dst_image_h.laddr[0]=(uintptr_t)(ndst->kbuffer?ndst->kbuffer:ndst->buffer);
+        blt.dst_image_h.format =G2D_FORMAT_RGB888;
+        blt.dst_image_h.alpha = 255;
+        blt.dst_image_h.use_phy_addr = (ndst->kbuffer!=NULL);
+        blt.dst_image_h.color = 0xee8899;
 
         if (ioctl(dev->g2d, G2D_CMD_BITBLT_H, &blt)< 0){
             LOGE("Error: G2D CMD BITBLT H failed");
