51cf33a3f284f62cab84b6130b3ea29902c9f2ae
侯歌 <hou731080@163.com>
2024-11-19 16:16:40 +0800

    Window::getCanvas change ANTILIZE_DEFAULT

--- src/gui/widget/cdwindow.cc
+++ src/gui/widget/cdwindow.cc
@@ -292,7 +292,7 @@ RefPtr<Canvas>Window::getCanvas(){
         LOGV("rotation=%d window.size=%dx%d canvas.size=%dx%d antialias=%d,%d,%d,%d,%d,%d,%d",rotation*90,getWidth(),getHeight(),
              canvasWidth,canvasHeight,canvas->get_antialias(),CAIRO_ANTIALIAS_NONE,CAIRO_ANTIALIAS_GRAY,CAIRO_ANTIALIAS_SUBPIXEL,
              CAIRO_ANTIALIAS_FAST,CAIRO_ANTIALIAS_GOOD,CAIRO_ANTIALIAS_BEST);
-        canvas->set_antialias(Cairo::ANTIALIAS_NONE);
+        //canvas->set_antialias(Cairo::ANTIALIAS_NONE);
         switch(rotation){
         case Display::ROTATION_0:break;
         case Display::ROTATION_90:
