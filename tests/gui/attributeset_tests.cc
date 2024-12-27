7cee8f9c3a6d2f3b41b9f4e2a347ca1d8094c26a
侯歌 <hou731080@163.com>
2024-12-27 21:44:10 +0800

    modify attributeset_tests.cc add some testcase

--- tests/gui/attributeset_tests.cc
+++ tests/gui/attributeset_tests.cc
@@ -3,7 +3,7 @@
 
 using namespace cdroid;
 
-class ATTRIBUTESET:public testing::Test{
+class ATTS:public testing::Test{
 
    public :
    virtual void SetUp(){
@@ -12,39 +12,43 @@ class ATTRIBUTESET:public testing::Test{
    }
 };
 
-TEST_F(ATTRIBUTESET,normalize){
-   ASSERT_EQ(AttributeSet::normalize("cdroid","?textColor"),"cdroid:attr/textColor");
-   ASSERT_EQ(AttributeSet::normalize("cdroid","?attr/textColor"),"cdroid:attr/textColor");
-   ASSERT_EQ(AttributeSet::normalize("cdroid","?cdroid:attr/textColor"),"cdroid:attr/textColor");
+TEST_F(ATTS,normalize){
+    ASSERT_EQ(AttributeSet::normalize("cdroid","red"),"red");
+    ASSERT_EQ(AttributeSet::normalize("cdroid","#123456"),"#123456");
+    ASSERT_EQ(AttributeSet::normalize("cdroid","@null"),"null");
+    ASSERT_EQ(AttributeSet::normalize("cdroid","@mipmap/test"),"cdroid:mipmap/test");
+    ASSERT_EQ(AttributeSet::normalize("cdroid","?textColor"),"cdroid:attr/textColor");
+    ASSERT_EQ(AttributeSet::normalize("cdroid","?attr/textColor"),"cdroid:attr/textColor");
+    ASSERT_EQ(AttributeSet::normalize("cdroid","?cdroid:attr/textColor"),"cdroid:attr/textColor");
 }
 
-TEST_F(ATTRIBUTESET,inherit1){
-   AttributeSet att1(nullptr,"cdroid");
-   AttributeSet att2(nullptr,"cdroid");
-   const char*kvs[]={
-       "color1","?textColor",
-       "color2","?attr/textColor",
-       "color3","?cdroid:attr/textColor",
-       nullptr
-   };
-   att1.set(kvs);
-   att2.inherit(att1);
-   ASSERT_EQ(att2.getString("color1"),"cdroid:attr/textColor");
-   ASSERT_EQ(att2.getString("color2"),"cdroid:attr/textColor");
-   ASSERT_EQ(att2.getString("color3"),"cdroid:attr/textColor");
+TEST_F(ATTS,inherit1){
+    AttributeSet att1(nullptr,"cdroid");
+    AttributeSet att2(nullptr,"cdroid");
+    const char*kvs[]={
+        "color1","?textColor",
+        "color2","?attr/textColor",
+        "color3","?cdroid:attr/textColor",
+        nullptr
+    };
+    att1.set(kvs);
+    att2.inherit(att1);
+    ASSERT_EQ(att2.getString("color1"),"cdroid:attr/textColor");
+    ASSERT_EQ(att2.getString("color2"),"cdroid:attr/textColor");
+    ASSERT_EQ(att2.getString("color3"),"cdroid:attr/textColor");
 }
-TEST_F(ATTRIBUTESET,inherit2){
-   AttributeSet att1(nullptr,"cdroid");
-   AttributeSet att2(nullptr,"app");
-   const char*kvs[]={
-       "color1","?textColor",
-       "color2","?attr/textColor",
-       "color3","?cdroid:attr/textColor",
-       nullptr
-   };
-   att1.set(kvs);
-   att2.inherit(att1);
-   ASSERT_EQ(att2.getString("color1"),"cdroid:attr/textColor");
-   ASSERT_EQ(att2.getString("color2"),"cdroid:attr/textColor");
-   ASSERT_EQ(att2.getString("color3"),"cdroid:attr/textColor");
+TEST_F(ATTS,inherit2){
+    AttributeSet att1(nullptr,"cdroid");
+    AttributeSet att2(nullptr,"app");
+    const char*kvs[]={
+        "color1","?textColor",
+        "color2","?attr/textColor",
+        "color3","?cdroid:attr/textColor",
+        nullptr
+    };
+    att1.set(kvs);
+    att2.inherit(att1);
+    ASSERT_EQ(att2.getString("color1"),"cdroid:attr/textColor");
+    ASSERT_EQ(att2.getString("color2"),"cdroid:attr/textColor");
+    ASSERT_EQ(att2.getString("color3"),"cdroid:attr/textColor");
 }
