25533dd9fa8d932fc3662022dece462d57dab817
侯歌 <hou731080@163.com>
2025-01-14 09:47:09 +0800

    fix touchdevice's virtual key

--- src/gui/core/attributeset.cc
+++ src/gui/core/attributeset.cc
@@ -37,7 +37,7 @@ std::string AttributeSet::normalize(const std::string&pkg,const std::string&prop
     const bool hasColon = property.find(':')!=std::string::npos;
     const bool hasAT = property.size() && (property[0]=='@');
     if(hasColon&&(hasAT==false)) {
-        if(property.compare(0,7,"android")==0){
+        if(property.compare(0,8,"android:")==0){
             std::string value =property;
             value[1]='c';/*android cahnge to cdroid*/
             return value.substr(1);
