bbb3a3179481996eaa6a99fcd09745ee82688ccd
侯歌 <hou731080@163.com>
2025-02-11 18:04:31 +0800

    modify samples/handler add handleMessages support

--- apps/samples/handler.cc
+++ apps/samples/handler.cc
@@ -1,12 +1,19 @@
 #include <cdroid.h>
 #include <unistd.h>
 #include <thread>
+class MyHandler:public Handler{
+public:
+    MyHandler():Handler(){}
+    void handleMessage(Message& msg)override{
+        LOGD("handle message %d",msg.what);
+    }
+};
 int main(int argc,const char*argv[]){
     App app(argc,argv);
     Window*w= new Window(0,0,-1,-1);
     TextView*tv=new TextView("Hello world!",600,40);
     ProgressBar*pb = new ProgressBar(600,40);
-    Handler*handler=new Handler();
+    Handler*handler=new MyHandler();
     tv->setTextColor(0xFFFFFFFF);
     tv->setBackgroundColor(0xFF334455);
     tv->setTextSize(32);
@@ -28,6 +35,14 @@ int main(int argc,const char*argv[]){
            usleep(10000);
         }
     });
+    std::thread th2([&](){
+        int what=0;
+        while(1){
+           handler->sendEmptyMessage(what++);
+           usleep(1000*500);
+        }
+    });
     th.detach();
+    th2.detach();
     return app.exec();
 }
