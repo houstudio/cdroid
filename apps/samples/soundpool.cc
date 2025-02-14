58fc93e3f0d80d4210093dadda529a6bae85273d
侯歌 <hou731080@163.com>
2025-02-14 15:54:47 +0800

    modify apps/samples/soundpool.cc

--- apps/samples/soundpool.cc
+++ apps/samples/soundpool.cc
@@ -1,10 +1,11 @@
 #include <media/soundpool.h>
-
+#include <unistd.h>
 int main(int argc,char*argv[]){
     cdroid::SoundPool sp;
     if(argc>1){
         int sid =sp.load(argv[1]);
         sp.play(sid);
     }
+    while(1)sleep(1);
     return 0;
 }
