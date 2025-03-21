83d67ba3eec39e82a3188d6610eae9afe0f14371
侯歌 <hou731080@163.com>
2025-03-21 19:26:09 +0800

    update samples/vectordrawable.cc

--- apps/samples/vectordrawable.cc
+++ apps/samples/vectordrawable.cc
@@ -1,7 +1,24 @@
 #include <drawables/vectordrawable.h>
-
-int main(int argc,char*argv[]){
-    if(argc>1)
-        cdroid::VectorDrawable::create(nullptr,argv[1]);
+#include <drawables/drawableinflater.h>
+#include <cdroid.h>
+int main(int argc,const char*argv[]){
+    App app(argc,argv);
+    Window*w=new Window(0,0,-1,-1);
+    TextView*tv=new TextView("AnimatedVectorDrawable testcase",0,0);
+    w->setBackgroundColor(0xFF112233);
+    w->addView(tv);
+    Drawable *d = nullptr;
+    /*Animator*anim=AnimatorInflater::loadAnimator(&app,"cdroid:anim/btn_radio_to_off_mtrl_ring_outer_animation");
+    Animator*anim2=anim->clone();
+    LOGD("anim=%p anim2=%d",anim,anim2);
+    delete anim;
+    delete anim2;*/
+    if(argc>1)d=DrawableInflater::loadDrawable(&app,argv[1]);
+    else d= DrawableInflater::loadDrawable(&app,"@cdroid:drawable/btn_radio_on_to_off_mtrl_animation");
+    tv->setBackground(d);
+    if(dynamic_cast<AnimatedVectorDrawable*>(d)){
+        ((AnimatedVectorDrawable*)d)->start();
+    }
+    app.exec();
     return 0;
 }
