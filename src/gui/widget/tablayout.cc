5a984432bcb381726e96cb5ed7f25f818f3588ef
侯歌 <hou731080@163.com>
2025-04-24 09:54:24 +0800

    fix tablayout.cc's crash

--- src/gui/widget/tablayout.cc
+++ src/gui/widget/tablayout.cc
@@ -65,7 +65,7 @@ TabLayout::TabLayout(Context*context,const AttributeSet&atts)
 }
 
 TabLayout::~TabLayout(){
-    delete mTabTextColors;
+    //delete mTabTextColors;/*ColorStateList is global holded by keymap ,cant destroied*/
     delete mScrollAnimator;
     delete mAdapterChangeListener;
     delete mPagerAdapterObserver;
