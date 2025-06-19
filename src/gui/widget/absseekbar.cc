29a5d0e9cbe7d876ee9ef816395e027deba667eb
侯歌 <hou731080@163.com>
2025-06-19 06:21:49 +0000

    :sparkles: absseekbar add thumbtint support

--- src/gui/widget/absseekbar.cc
+++ src/gui/widget/absseekbar.cc
@@ -23,7 +23,6 @@ AbsSeekBar::AbsSeekBar(Context*ctx,const AttributeSet&attrs):ProgressBar(ctx,att
     const bool useDisabledAlpha = attrs.getBoolean("useDisabledAlpha", true);
     mDisabledAlpha = useDisabledAlpha?attrs.getFloat("disabledAlpha", 0.5f):1.f;
     mSplitTrack = attrs.getBoolean("splitTrack",false);
-    const bool useDisableAlphs = attrs.getBoolean("useDisableAlpha",true);
     mThumbExclusionMaxSize = ctx->getDimension("cdroid:dimen/seekbar_thumb_exclusion_max_size");
 
     applyThumbTint();
