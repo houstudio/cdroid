a8524be36617869cd7be627656d588603596d37d
侯歌 <hou731080@163.com>
2025-06-26 16:50:58 +0800

    :construction: add ENABLE_I18N option

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -38,6 +38,7 @@ option(ENABLE_RECYCLERVIEW "Enable RecyclerView" ON)
 option(ENABLE_NAVIGATION "Enable Navigation" OFF)
 option(ENABLE_TRANSITION "Enable Transition scene animation(TODO...)" OFF)
 option(ENABLE_FLEXBOXLAYOUT "Enable FlexboxLayout" OFF)
+option(ENABLE_I18N "Enable I18N" OFF)
 
 cmake_dependent_option(ENABLE_GIF "enable gif encode and decoder" ON "GIF_FOUND" OFF)
 cmake_dependent_option(ENABLE_JPEG "enable jpeg decoder" ON "JPEG_FOUND" OFF)
