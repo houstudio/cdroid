ceb100e8268d7ec702d33715478e3122771a954c
侯歌 <hou731080@163.com>
2025-07-01 09:50:35 +0800

    :sparkles: add core/uri.cc

---
+++ src/gui/core/uri.cc
@@ -0,0 +1,526 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <regex>
+#include <vector>
+#include <climits>
+#include <core/uri.h>
+#include <porting/cdlog.h>
+
+namespace cdroid {
+namespace {
+    const std::string NOT_CACHED = "NOT VALID";
+    const std::string EMPTY = "";
+    const size_t NOT_FOUND = std::string::npos;
+    const int NOT_CALCULATED = -2;
+    const int PORT_NONE = -1;
+    const char SCHEME_SEPARATOR = ':';
+    const char SCHEME_FRAGMENT = '#';
+    const char LEFT_SEPARATOR = '/';
+    const char RIGHT_SEPARATOR = '\\';
+    const char QUERY_FLAG = '?';
+    const char USER_HOST_SEPARATOR = '@';
+    const char PORT_SEPARATOR = ':';
+    const size_t POS_INC = 1;
+    const size_t POS_INC_MORE = 2;
+    const size_t POS_INC_AGAIN = 3;
+}; // namespace
+
+Uri::Uri(const std::string& uriString)
+{
+    cachedSsi_ = NOT_FOUND;
+    cachedFsi_ = NOT_FOUND;
+    port_ = NOT_CALCULATED;
+
+    if (uriString.empty()) {
+        return;
+    }
+
+    uriString_ = uriString;
+    scheme_ = NOT_CACHED;
+    ssp_ = NOT_CACHED;
+    authority_ = NOT_CACHED;
+    host_ = NOT_CACHED;
+    userInfo_ = NOT_CACHED;
+    query_ = NOT_CACHED;
+    path_ = NOT_CACHED;
+    fragment_ = NOT_CACHED;
+
+    if (!checkScheme()) {
+        uriString_ = EMPTY;
+        LOGE("URI Scheme wrong");
+    }
+}
+
+Uri::~Uri()
+{}
+
+bool Uri::checkScheme()
+{
+    scheme_ = parseScheme();
+    if (scheme_.empty()) {
+        return true;
+    }
+    try {
+        std::regex schemeRegex("[a-zA-Z][a-zA-Z|\\d|\\+|\\-|.]*$");
+        if (!std::regex_match(scheme_, schemeRegex)) {
+            return false;
+        }
+    } catch (std::regex_error &message) {
+        LOGE("regex fail,message:%s", message.what());
+        return false;
+    }
+    return true;
+}
+
+std::string Uri::getScheme()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    if (scheme_ == NOT_CACHED) {
+        scheme_ = parseScheme();
+    }
+    return scheme_;
+}
+
+std::string Uri::parseScheme()
+{
+    size_t ssi = findSchemeSeparator();
+    return (ssi == NOT_FOUND) ? EMPTY : uriString_.substr(0, ssi);
+}
+
+std::string Uri::getSchemeSpecificPart()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    return (ssp_ == NOT_CACHED) ? (ssp_ = parseSsp()) : ssp_;
+}
+
+std::string Uri::parseSsp()
+{
+    size_t ssi = findSchemeSeparator();
+    size_t fsi = findFragmentSeparator();
+
+    size_t start = (ssi == NOT_FOUND) ? 0 : (ssi + 1);
+    size_t end = (fsi == NOT_FOUND) ? uriString_.size() : fsi;
+
+    // Return everything between ssi and fsi.
+    std::string ssp = EMPTY;
+    if (end > start) {
+        ssp = uriString_.substr(start, end - start);
+    }
+
+    return ssp;
+}
+
+std::string Uri::getAuthority()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    if (authority_ == NOT_CACHED) {
+        authority_ = parseAuthority();
+    }
+    return authority_;
+}
+
+std::string Uri::parseAuthority()
+{
+    size_t ssi = findSchemeSeparator();
+    if (ssi == NOT_FOUND) {
+        return EMPTY;
+    }
+
+    size_t length = uriString_.length();
+    // If "//" follows the scheme separator, we have an authority.
+    if ((length > (ssi + POS_INC_MORE)) && (uriString_.at(ssi + POS_INC) == LEFT_SEPARATOR) &&
+        (uriString_.at(ssi + POS_INC_MORE) == LEFT_SEPARATOR)) {
+        // Look for the start of the path, query, or fragment, or the end of the string.
+        size_t start = ssi + POS_INC_AGAIN;
+        size_t end = start;
+
+        while (end < length) {
+            char ch = uriString_.at(end);
+            if ((ch == LEFT_SEPARATOR) || (ch == RIGHT_SEPARATOR) || (ch == QUERY_FLAG) ||
+                (ch == SCHEME_FRAGMENT)) {
+                break;
+            }
+
+            end++;
+        }
+
+        return uriString_.substr(start, end - start);
+    } else {
+        return EMPTY;
+    }
+}
+
+std::string Uri::getUserInfo()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    if (userInfo_ == NOT_CACHED) {
+        userInfo_ = parseUserInfo();
+    }
+    return userInfo_;
+}
+
+std::string Uri::parseUserInfo()
+{
+    std::string authority = getAuthority();
+    if (authority.empty()) {
+        return EMPTY;
+    }
+
+    size_t end = authority.find_last_of(USER_HOST_SEPARATOR);
+    return (end == NOT_FOUND) ? EMPTY : authority.substr(0, end);
+}
+
+std::string Uri::getHost()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    if (host_ == NOT_CACHED) {
+        host_ = parseHost();
+    }
+    return host_;
+}
+
+std::string Uri::parseHost()
+{
+    std::string authority = getAuthority();
+    if (authority.empty()) {
+        return EMPTY;
+    }
+
+    // Parse out user info and then port.
+    size_t userInfoSeparator = authority.find_last_of(USER_HOST_SEPARATOR);
+    size_t start = (userInfoSeparator == NOT_FOUND) ? 0 : (userInfoSeparator + 1);
+    size_t portSeparator = authority.find_first_of(PORT_SEPARATOR, start);
+    size_t end = (portSeparator == NOT_FOUND) ? authority.size() : portSeparator;
+
+    std::string host = EMPTY;
+    if (start < end) {
+        host = authority.substr(start, end - start);
+    }
+
+    return host;
+}
+
+int Uri::getPort()
+{
+    if (uriString_.empty()) {
+        return PORT_NONE;
+    }
+
+    if (port_ == NOT_CALCULATED) {
+        port_ = parsePort();
+    }
+    return port_;
+}
+
+static bool StrToInt(const std::string& str, int& value)
+{
+    if (str.empty() || (!isdigit(str.front()) && (str.front() != '-'))) {
+        return false;
+    }
+
+    char* end = nullptr;
+    errno = 0;
+    auto addr = str.c_str();
+    auto result = strtol(addr, &end, 10); /* 10 means decimal */
+    if ((end == addr) || (end[0] != '\0') || (errno == ERANGE) ||
+            (result > INT_MAX) || (result < INT_MIN)) {
+        return false;
+    }
+    value = static_cast<int>(result);
+    return true;
+}
+
+int Uri::parsePort()
+{
+    std::string authority = getAuthority();
+    if (authority.empty()) {
+        return PORT_NONE;
+    }
+
+    // Make sure we look for the port separtor *after* the user info separator.
+    size_t userInfoSeparator = authority.find_last_of(USER_HOST_SEPARATOR);
+    size_t start = (userInfoSeparator == NOT_FOUND) ? 0 : (userInfoSeparator + 1);
+    size_t portSeparator = authority.find_first_of(PORT_SEPARATOR, start);
+    if (portSeparator == NOT_FOUND) {
+        return PORT_NONE;
+    }
+
+    start = portSeparator + 1;
+    std::string portString = authority.substr(start);
+
+    int value = PORT_NONE;
+    return StrToInt(portString, value) ? value : PORT_NONE;
+}
+
+std::string Uri::getQuery()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    if (query_ == NOT_CACHED) {
+        query_ = parseQuery();
+    }
+    return query_;
+}
+
+std::string Uri::parseQuery()
+{
+    size_t ssi = findSchemeSeparator();
+    if (ssi == NOT_FOUND) {
+        ssi = 0;
+    }
+    size_t qsi = uriString_.find_first_of(QUERY_FLAG, ssi);
+    if (qsi == NOT_FOUND) {
+        return EMPTY;
+    }
+
+    size_t start = qsi + 1;
+    size_t fsi = findFragmentSeparator();
+    if (fsi == NOT_FOUND) {
+        return uriString_.substr(start);
+    }
+
+    if (fsi < qsi) {
+        // Invalid.
+        return EMPTY;
+    }
+
+    return uriString_.substr(start, fsi - start);
+}
+
+std::string Uri::getPath()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    if (path_ == NOT_CACHED) {
+        path_ = parsePath();
+    }
+    return path_;
+}
+
+void Uri::getPathSegments(std::vector<std::string>& segments)
+{
+    if (uriString_.empty()) {
+        return;
+    }
+    if (path_ == NOT_CACHED) {
+        path_ = parsePath();
+    }
+
+    size_t previous = 0;
+    size_t current;
+    while ((current = path_.find(LEFT_SEPARATOR, previous)) != std::string::npos) {
+        if (previous < current) {
+            segments.emplace_back(path_.substr(previous, current - previous));
+        }
+        previous = current + POS_INC;
+    }
+    // Add in the final path segment.
+    if (previous < path_.length()) {
+        segments.emplace_back(path_.substr(previous));
+    }
+}
+
+std::string Uri::parsePath()
+{
+    size_t ssi = findSchemeSeparator();
+    // If the URI is absolute.
+    if (ssi != NOT_FOUND) {
+        // Is there anything after the ':'?
+        if ((ssi + 1) == uriString_.length()) {
+            // Opaque URI.
+            return EMPTY;
+        }
+
+        // A '/' after the ':' means this is hierarchical.
+        if (uriString_.at(ssi + 1) != LEFT_SEPARATOR) {
+            // Opaque URI.
+            return EMPTY;
+        }
+    } else {
+        // All relative URIs are hierarchical.
+    }
+
+    return parsePath(ssi);
+}
+
+std::string Uri::parsePath(size_t ssi)
+{
+    size_t length = uriString_.length();
+
+    // Find start of path.
+    size_t pathStart = (ssi == NOT_FOUND) ? 0 : (ssi + POS_INC);
+    if ((length > (pathStart + POS_INC)) && (uriString_.at(pathStart) == LEFT_SEPARATOR) &&
+        (uriString_.at(pathStart + POS_INC) == LEFT_SEPARATOR)) {
+        // Skip over authority to path.
+        pathStart += POS_INC_MORE;
+
+        while (pathStart < length) {
+            char ch = uriString_.at(pathStart);
+            if ((ch == QUERY_FLAG) || (ch == SCHEME_FRAGMENT)) {
+                return EMPTY;
+            }
+
+            if ((ch == LEFT_SEPARATOR) || (ch == RIGHT_SEPARATOR)) {
+                break;
+            }
+
+            pathStart++;
+        }
+    }
+
+    // Find end of path.
+    size_t pathEnd = pathStart;
+    while (pathEnd < length) {
+        char ch = uriString_.at(pathEnd);
+        if ((ch == QUERY_FLAG) || (ch == SCHEME_FRAGMENT)) {
+            break;
+        }
+
+        pathEnd++;
+    }
+
+    return uriString_.substr(pathStart, pathEnd - pathStart);
+}
+
+std::string Uri::getFragment()
+{
+    if (uriString_.empty()) {
+        return EMPTY;
+    }
+
+    if (fragment_ == NOT_CACHED) {
+        fragment_ = parseFragment();
+    }
+    return fragment_;
+}
+
+std::string Uri::parseFragment()
+{
+    size_t fsi = findFragmentSeparator();
+    return (fsi == NOT_FOUND) ? EMPTY : uriString_.substr(fsi + 1);
+}
+
+size_t Uri::findSchemeSeparator()
+{
+    if (cachedSsi_ == NOT_FOUND) {
+        cachedSsi_ = uriString_.find_first_of(SCHEME_SEPARATOR);
+    }
+    return cachedSsi_;
+}
+
+size_t Uri::findFragmentSeparator()
+{
+    if (cachedFsi_ == NOT_FOUND) {
+        cachedFsi_ = uriString_.find_first_of(SCHEME_FRAGMENT, findSchemeSeparator());
+    }
+    return cachedFsi_;
+}
+
+bool Uri::isHierarchical()
+{
+    if (uriString_.empty()) {
+        return false;
+    }
+
+    size_t ssi = findSchemeSeparator();
+    if (ssi == NOT_FOUND) {
+        // All relative URIs are hierarchical.
+        return true;
+    }
+
+    if (uriString_.length() == (ssi + 1)) {
+        // No ssp.
+        return false;
+    }
+
+    // If the ssp starts with a '/', this is hierarchical.
+    return (uriString_.at(ssi + 1) == LEFT_SEPARATOR);
+}
+
+bool Uri::isAbsolute()
+{
+    if (uriString_.empty()) {
+        return false;
+    }
+
+    return !isRelative();
+}
+
+bool Uri::isRelative()
+{
+    if (uriString_.empty()) {
+        return false;
+    }
+
+    // Note: We return true if the index is 0
+    return findSchemeSeparator() == NOT_FOUND;
+}
+
+bool Uri::equals(const Uri& other) const
+{
+    return uriString_ == other.toString();
+}
+
+int Uri::compareTo(const Uri& other) const
+{
+    return uriString_.compare(other.toString());
+}
+
+std::string Uri::toString() const
+{
+    return uriString_;
+}
+
+bool Uri::operator==(const Uri& other) const
+{
+    return uriString_ == other.toString();
+}
+#if 0
+bool Uri::marshalling(Parcel& parcel) const
+{
+    if (isAsciiString(uriString_)) {
+        return parcel.WriteString16(Str8ToStr16(uriString_));
+    }
+
+    LOGE("uriString_ is not ascii string");
+    return false;
+}
+
+Uri* Uri::unmarshalling(Parcel& parcel)
+{
+    return new Uri(Str16ToStr8(parcel.ReadString16()));
+}
+#endif
+} // namespace cdroid
---
+++ src/gui/core/uri.h
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ABILITY_BASE_URI_H__
+#define __ABILITY_BASE_URI_H__
+
+#include <string>
+#include <vector>
+#include "parcel.h"
+
+namespace cdroid {
+class Uri/*: public Parcelable*/{
+public:
+    explicit Uri(const std::string& uriString);
+    ~Uri();
+
+    /**
+     * Get the Scheme part.
+     *
+     * @return the scheme string.
+     */
+    std::string getScheme();
+
+    /**
+     * Get the Ssp part.
+     *
+     * @return the SchemeSpecificPart string.
+     */
+    std::string getSchemeSpecificPart();
+
+    /**
+     * Get the GetAuthority part.
+     *
+     * @return the authority string.
+     */
+    std::string getAuthority();
+
+    /**
+     * Get the Host part.
+     *
+     * @return the host string.
+     */
+    std::string getHost();
+
+    /**
+     * Get the Port part.
+     *
+     * @return the port number.
+     */
+    int getPort();
+
+    /**
+     * Get the User part.
+     *
+     * @return the user string.
+     */
+    std::string getUserInfo();
+
+    /**
+     * Get the Query part.
+     *
+     * @return the query string.
+     */
+    std::string getQuery();
+
+    /**
+     * Get the Path part.
+     *
+     * @return the path string.
+     */
+    std::string getPath();
+
+    /**
+     * Get the path segments.
+     *
+     * @param the path segments of Uri.
+     */
+    void getPathSegments(std::vector<std::string>& segments);
+
+    /**
+     * Get the Fragment part.
+     *
+     * @return the fragment string.
+     */
+    std::string getFragment();
+
+    /**
+     * Returns true if this URI is hierarchical like "http://www.example.com".
+     * Absolute URIs are hierarchical if the scheme-specific part starts with a '/'.
+     * Relative URIs are always hierarchical.
+     *
+     * @return true if this URI is hierarchical, false if it's opaque.
+     */
+    bool isHierarchical();
+
+    /**
+     * Returns true if this URI is absolute, i.e.&nbsp;if it contains an explicit scheme.
+     *
+     * @return true if this URI is absolute, false if it's relative.
+     */
+    bool isAbsolute();
+
+    /**
+     * Returns true if this URI is relative, i.e.&nbsp;if it doesn't contain an explicit scheme.
+     *
+     * @return true if this URI is relative, false if it's absolute.
+     */
+    bool isRelative();
+
+    /**
+     * Check whether the other is the same as this.
+     *
+     * @return true if the same string.
+     */
+    bool equals(const Uri& other) const;
+
+    /**
+     * Compare to other uri.
+     *
+     * @return the string compare result.
+     */
+    int compareTo(const Uri& other) const;
+
+    /**
+     * Convert to a string object.
+     *
+     * @return a string object.
+     */
+    std::string toString() const;
+
+    /**
+     * override the == method.
+     *
+     * @return true if the same content, false not the same content.
+     */
+    bool operator==(const Uri& other) const;
+
+    /**
+     * Override Parcelable' interface.
+     *
+     * @return true if parcel write success, false write fail.
+     */
+    //virtual bool marshalling(Parcel& parcel) const override;
+
+    /**
+     * Support the Ummarshlling method for construct object by  Parcel read.
+     *
+     * @return the uri object address.
+     */
+    //static Uri* Unmarshalling(Parcel& parcel);
+
+private:
+    bool checkScheme();
+    std::string parseScheme();
+    std::string parseSsp();
+    std::string parseAuthority();
+    std::string parseUserInfo();
+    std::string parseHost();
+    int parsePort();
+    std::string parsePath(size_t ssi);
+    std::string parsePath();
+    std::string parseQuery();
+    std::string parseFragment();
+
+    /**
+    * Finds the first ':'.
+    *
+    * @return the pos of the ':', string::npos if none found.
+    */
+    size_t findSchemeSeparator();
+
+    /**
+     * Finds the first '#'.
+     *
+     * @return the pos of the '#', string::npos if none found.
+     */
+    size_t findFragmentSeparator();
+
+    std::string uriString_;
+    std::string scheme_;
+    std::string ssp_;
+    std::string authority_;
+    std::string host_;
+    int port_;
+    std::string userInfo_;
+    std::string query_;
+    std::string path_;
+    std::string fragment_;
+    size_t cachedSsi_;
+    size_t cachedFsi_;
+};
+} // namespace cdroid
+#endif // __ABILITY_BASE_URI_H__
