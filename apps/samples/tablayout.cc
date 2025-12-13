f0767cffec9abd8c2a02d09940f04b6ccfc9eb1b
侯歌 <hou731080@163.com>
2025-12-13 21:43:37 +0800

    :construction:modify samples/tablayout

--- apps/samples/tablayout.cc
+++ apps/samples/tablayout.cc
@@ -1,57 +1,41 @@
 #include <cdroid.h>
-class MyPageAdapter:public PagerAdapter{
-public:
-    int getCount()override{return 8;}
-    bool isViewFromObject(View* view, void*object)override{ return view==object;}
-    void* instantiateItem(ViewGroup* container, int position)override{
-#if ENABLE(DAYTIME_WIDGETS)
-        SimpleMonthView*sm=new  SimpleMonthView(1280,560);
-        sm->setMonthParams(23,Calendar::MAY+position,2021,-1,1,31);
-        container->addView(sm);
-        sm->setId(100+position);
-        return sm;
-#else
-        return new TextView("TextView",0,0);
-#endif
-    }
-    void destroyItem(ViewGroup* container, int position,void* object)override{
-        container->removeView((View*)object);
-        delete (View*)object;
-    }
-    std::string getPageTitle(int position)override{
-        return std::string("Tab")+std::to_string(position);
-    }
-    float getPageWidth(int position)override{return 1.f;}//if returned calue <1 OffscreenPageLimit must be larger to workfine
-};
-
-
+#include <porting/cdlog.h>
+#include <widget/candidateview.h>
+#include <widget/chronometer.h>
+#include <core/bundle.h>
+#include <core/process.h>
+#include <view/asyncinflater.h>
+#include <widget/tablayout.h>
+//#include <mgl2/mgl.h>
 int main(int argc,const char*argv[]){
     App app(argc,argv);
-    AnimationHandler::getInstance().setFrameDelay(200);
-    Window*w=new Window(0,0,1280,600);
-    MyPageAdapter*gpAdapter=new MyPageAdapter();
+    Window*w=new Window(0,0,800,600);
+    w->setBackgroundColor(0xFF4488cc);
+    TabLayout*tl =new TabLayout(800,600);
+    w->addView(tl,new Window::LayoutParams(LayoutParams::MATCH_PARENT,64));
+    for(int i=0;i<4;i++){
+       auto tab =tl->newTab();
+       tab->setText("tab");
+       tl->addTab(tab);
+    }
 
-    LinearLayout*layout=new LinearLayout(1280,600);
-    layout->setOrientation(LinearLayout::VERTICAL);
+    LinearLayout*llbtns=new LinearLayout(800,64);
+    llbtns->setBackgroundColor(0xFF112233);
+    llbtns->setOrientation(LinearLayout::HORIZONTAL);
+    Window::LayoutParams*wlp=new Window::LayoutParams(LayoutParams::MATCH_PARENT,LayoutParams::WRAP_CONTENT);
+    wlp->gravity=Gravity::BOTTOM;
+    w->addView(llbtns,wlp);
 
-    TabLayout* tab=new TabLayout(1280,36);
-    ViewPager* pager=new ViewPager(1280,560);
-    pager->setOffscreenPageLimit(gpAdapter->getCount());
-    pager->setOverScrollMode(View::OVER_SCROLL_ALWAYS);
-    pager->setAdapter(gpAdapter);
-    pager->setBackgroundColor(0xFFF3333);
-    tab->setSelectedTabIndicatorColor(0x8000FF00);
-    tab->setSelectedTabIndicatorHeight(4); 
-    tab->setTabIndicatorGravity(Gravity::BOTTOM);//TOP/BOTTOM/CENTER_VERTICAL/FILL_VERTICAL
-    tab->setupWithViewPager(pager);
-    layout->addView(tab);
-    tab->setId(1);
-    layout->addView(pager);
-    pager->setId(10);
-    tab->setTabTextColors(0xFFFF0000,0xFF00FF00);
-    w->addView(layout);
+    Button*btn=new Button("Add",120,48);
+    LinearLayout::LayoutParams*lp=new LinearLayout::LayoutParams(LayoutParams::WRAP_CONTENT,LayoutParams::WRAP_CONTENT);
+    lp->topMargin=200;
+    llbtns->addView(btn,lp);
+    btn->setOnClickListener([&](View&){
+      auto tab =tl->newTab();
+      tab->setText("tab");
+      tl->addTab(tab);
+      tl->requestLayout();
+        });
     w->requestLayout();
-    int result = app.exec();
-    delete gpAdapter;
-    return result;
+    return app.exec();
 }
