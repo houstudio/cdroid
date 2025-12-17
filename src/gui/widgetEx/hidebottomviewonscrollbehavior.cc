98c6959f429563dd48866166ff19e1f9178c4035
侯歌 <hou731080@163.com>
2025-12-17 18:40:48 +0800

    :construction: modify HideBottomViewOnScrollBehavior

--- src/gui/widgetEx/hidebottomviewonscrollbehavior.cc
+++ src/gui/widgetEx/hidebottomviewonscrollbehavior.cc
@@ -1,3 +1,20 @@
+/*********************************************************************************
+ * Copyright (C) [2019] [houzh@msn.com]
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *********************************************************************************/
 #include <widgetEx/hidebottomviewonscrollbehavior.h>
 namespace cdroid{
 HideBottomViewOnScrollBehavior::HideBottomViewOnScrollBehavior() {
@@ -27,7 +44,7 @@ bool HideBottomViewOnScrollBehavior::onLayoutChild(CoordinatorLayout& parent, Vi
 
 void HideBottomViewOnScrollBehavior::setAdditionalHiddenOffsetY(View& child, int offset) {
     mAdditionalHiddenOffsetY = offset;
-    if (mCurrentState == 1) {
+    if (mCurrentState == STATE_SCROLLED_DOWN) {
         child.setTranslationY((float)(mHeight + mAdditionalHiddenOffsetY));
     }
 
@@ -47,7 +64,7 @@ void HideBottomViewOnScrollBehavior::onNestedScroll(CoordinatorLayout& coordinat
 }
 
 bool HideBottomViewOnScrollBehavior::isScrolledUp() const{
-    return mCurrentState == 2;
+    return mCurrentState == STATE_SCROLLED_UP;
 }
 
 void HideBottomViewOnScrollBehavior::slideUp(View& child) {
@@ -61,7 +78,7 @@ void HideBottomViewOnScrollBehavior::slideUp(View& child, bool animate) {
             child.clearAnimation();
         }
 
-        updateCurrentState(child, 2);
+        updateCurrentState(child, STATE_SCROLLED_UP);
         int targetTranslationY = 0;
         if (animate) {
             animateChildTo(child, targetTranslationY, (long)mEnterAnimDuration, mEnterAnimInterpolator);
@@ -73,7 +90,7 @@ void HideBottomViewOnScrollBehavior::slideUp(View& child, bool animate) {
 }
 
 bool HideBottomViewOnScrollBehavior::isScrolledDown() const{
-    return mCurrentState == 1;
+    return mCurrentState == STATE_SCROLLED_DOWN;
 }
 
 void HideBottomViewOnScrollBehavior::slideDown(View& child) {
@@ -87,7 +104,7 @@ void HideBottomViewOnScrollBehavior::slideDown(View& child, bool animate) {
             child.clearAnimation();
         }
 
-        updateCurrentState(child, 1);
+        updateCurrentState(child, STATE_SCROLLED_DOWN);
         int targetTranslationY = mHeight + mAdditionalHiddenOffsetY;
         if (animate) {
             animateChildTo(child, targetTranslationY, (long)mExitAnimDuration, mExitAnimInterpolator);
@@ -129,10 +146,10 @@ void HideBottomViewOnScrollBehavior::addOnScrollStateChangedListener(const HideB
 }
 
 void HideBottomViewOnScrollBehavior::removeOnScrollStateChangedListener(const HideBottomViewOnScrollBehavior::OnScrollStateChangedListener& listener) {
-    /*auto it = std::find(mOnScrollStateChangedListeners.begin(),mOnScrollStateChangedListeners.end(),listener);
+    auto it = std::find(mOnScrollStateChangedListeners.begin(),mOnScrollStateChangedListeners.end(),listener);
     if(it!=mOnScrollStateChangedListeners.end()){
         mOnScrollStateChangedListeners.erase(it);
-    }*/
+    }
 }
 
 void HideBottomViewOnScrollBehavior::clearOnScrollStateChangedListeners() {
