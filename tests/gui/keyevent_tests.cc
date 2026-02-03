43a776b15f9022c5dd4ac3bd9e0d93932b1cd274
侯歌 <hou731080@163.com>
2026-02-03 11:47:14 +0800

    :construction: add testcase

--- tests/gui/keyevent_tests.cc
+++ tests/gui/keyevent_tests.cc
@@ -19,3 +19,10 @@ TEST_F(KEYEVENT,keyCodeFromString){
     EXPECT_EQ(KeyEvent::keyCodeFromString("KEYCODE_UNKNOWN"),KeyEvent::KEYCODE_UNKNOWN);
     EXPECT_EQ(KeyEvent::keyCodeFromString("KEYCODE_F24"),KeyEvent::KEYCODE_F24);
 }
+
+TEST_F(KEYEVENT,metaStateToString){
+    EXPECT_STREQ(KeyEvent::metaStateToString(KeyEvent::META_ALT_ON|KeyEvent::META_CAP_LOCKED).c_str(),
+            "META_ALT_ON|META_CAP_LOCKED");
+    EXPECT_STREQ(KeyEvent::metaStateToString(KeyEvent::META_ALT_LEFT_ON|KeyEvent::META_CTRL_RIGHT_ON).c_str(),
+            "META_ALT_LEFT_ON|META_CTRL_RIGHT_ON");
+}
