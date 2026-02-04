68c85d4ee3c757b749dd97f2577b50cdd7bc9a92
侯歌 <hou731080@163.com>
2026-02-04 09:40:15 +0800

    :construction:modify keyevent_tests

--- tests/gui/keyevent_tests.cc
+++ tests/gui/keyevent_tests.cc
@@ -29,16 +29,42 @@ TEST_F(KEYEVENT,metaStateToString){
             "META_ALT_LEFT_ON|META_CTRL_RIGHT_ON");
 }
 
+const char*const axisName[]={
+    "AXIS_X",    "AXIS_Y",    "AXIS_PRESSURE",    "AXIS_SIZE",
+    "AXIS_TOUCH_MAJOR",   "AXIS_TOUCH_MINOR",
+    "AXIS_TOOL_MAJOR",    "AXIS_TOOL_MINOR",
+    "AXIS_ORIENTATION",
+    "AXIS_VSCROLL",    "AXIS_HSCROLL",    "AXIS_Z",
+    "AXIS_RX",    "AXIS_RY",    "AXIS_RZ",
+    "AXIS_HAT_X",    "AXIS_HAT_Y",
+    "AXIS_LTRIGGER", "AXIS_RTRIGGER",
+    "AXIS_THROTTLE", "AXIS_RUDDER",
+    "AXIS_WHEEL",    "AXIS_GAS",
+    "AXIS_BRAKE",    "AXIS_DISTANCE",
+    "AXIS_TILT",     "AXIS_SCROLL",
+    "AXIS_RELATIVE_X",   "AXIS_RELATIVE_Y",
+    "AXIS_RESERVED_29",  "AXIS_RESERVED_30",   "AXIS_RESERVED_31",
+    "AXIS_GENERIC_1",    "AXIS_GENERIC_2",    "AXIS_GENERIC_3",
+    "AXIS_GENERIC_4",    "AXIS_GENERIC_5",    "AXIS_GENERIC_6",
+    "AXIS_GENERIC_7",    "AXIS_GENERIC_8",    "AXIS_GENERIC_9",
+    "AXIS_GENERIC_10",   "AXIS_GENERIC_11",   "AXIS_GENERIC_12",
+    "AXIS_GENERIC_13",   "AXIS_GENERIC_14",   "AXIS_GENERIC_15",
+    "AXIS_GENERIC_16",
+    "AXIS_GESTURE_X_OFFSET",    "AXIS_GESTURE_Y_OFFSET",
+    "AXIS_GESTURE_SCROLL_X_DISTANCE",    "AXIS_GESTURE_SCROLL_Y_DISTANCE",
+    "AXIS_GESTURE_PINCH_SCALE_FACTOR",   "AXIS_GESTURE_SWIPE_FINGER_COUNT"
+};
+
 TEST_F(KEYEVENT,axisToString){
-    EXPECT_STREQ(MotionEvent::axisToString(MotionEvent::AXIS_X).c_str(),"AXIS_X");
-    EXPECT_STREQ(MotionEvent::axisToString(MotionEvent::AXIS_Y).c_str(),"AXIS_Y");
-    EXPECT_STREQ(MotionEvent::axisToString(MotionEvent::AXIS_PRESSURE).c_str(),"AXIS_PRESSURE");
+    for(int i=MotionEvent::AXIS_X;i<=MotionEvent::AXIS_GESTURE_SWIPE_FINGER_COUNT;i++){
+       EXPECT_STREQ(MotionEvent::axisToString(i).c_str(),axisName[i]);
+    }
 }
 
 TEST_F(KEYEVENT,axisFromString){
-    EXPECT_EQ(MotionEvent::axisFromString("AXIS_X"),MotionEvent::AXIS_X);
-    EXPECT_EQ(MotionEvent::axisFromString("AXIS_Y"),MotionEvent::AXIS_Y);
-    EXPECT_EQ(MotionEvent::axisFromString("AXIS_PRESSURE"),MotionEvent::AXIS_PRESSURE);
+    for(int i=MotionEvent::AXIS_X;i<=MotionEvent::AXIS_GESTURE_SWIPE_FINGER_COUNT;i++){
+        EXPECT_EQ(MotionEvent::axisFromString(axisName[i]),i);
+    }
 }
 
 
