64ce06a0b08127cec2ec79f7d1c0bad1fbcde9e0
侯歌 <hou731080@163.com>
2026-03-19 16:05:27 +0800

    :construction:modify changelog

--- apps/samples/cardview.cc
+++ apps/samples/cardview.cc
@@ -6,11 +6,10 @@ int main(int argc,const char*argv[]){
     App app(argc,argv);
     Window*w=new Window(0,0,-1,-1);
     CardView*cv=new CardView(0,0);
-    cv->setRadius(120);
-    cv->setMaxCardElevation(40);//MaxCardElevation must be set before setCardElevation
-    cv->setCardElevation(40);//
+    cv->setRadius(40);
+    cv->setMaxCardElevation(40);//MaxCardElevation must be set before setCardElevation or setElevation
+    cv->setElevation(40);//
     cv->setCardBackgroundColor(0xff00ff00);
-    cv->setContentPadding(40,40,40,40);
     w->setBackgroundColor(0xFFFFFFFF);
     w->addView(cv);
     return app.exec();
