7b7bb9f8922893abaf637d1bee15f8950d1fdf6e
侯歌 <hou731080@163.com>
2026-07-31 13:14:55 +0800

    :modify liccense desc

--- src/gui/navigation/fragmentnavigator.cc
+++ src/gui/navigation/fragmentnavigator.cc
@@ -1,3 +1,20 @@
+/*********************************************************************************
+ * Copyright (C) [2019] [houzh@msn.com]
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *********************************************************************************/
 #include <navigation/fragmentnavigator.h>
 #include <fragment/fragment.h>
 #include <fragment/fragmentfactory.h>
@@ -18,7 +35,7 @@ NavDestination* FragmentNavigator::createDestination(){
 void FragmentNavigator::Destination::onInflate(cdroid::Context* context, const AttributeSet& attrs){
     NavDestination::onInflate(context, attrs);
     setClassName(attrs.getString("name"));
-    LOGD("FragmentNavigator.Destination.onInflate route='%s' className='%s'",
+    LOGV("FragmentNavigator.Destination.onInflate route='%s' className='%s'",
          getRoute().c_str(), getClassName().c_str());
 }
 
@@ -29,7 +46,6 @@ void FragmentNavigator::navigate(NavDestination* destination, Bundle* args, NavO
     if(!d || !mFragmentManager){ LOGD("FragmentNavigator.navigate: bail (d=%p fm=%p)", d, mFragmentManager); return; }
     fragment::FragmentFactory factory;
     fragment::Fragment* fragment = factory.instantiate(d->getClassName());
-    LOGD("FragmentNavigator.navigate instantiate=%p", fragment);
     if(!fragment) return;
     fragment->setArguments(args ? new Bundle(*args) : nullptr);
     fragment::FragmentTransaction* t = mFragmentManager->beginTransaction();
