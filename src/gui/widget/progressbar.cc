dbfca485f239a6b93a4142e390d9c6501aebff29
侯歌 <hou731080@163.com>
2026-08-17 09:09:25 +0800

    :construction:modify gui/res/values/attrs_manifest.xml

--- src/gui/widget/progressbar.cc
+++ src/gui/widget/progressbar.cc
@@ -506,7 +506,7 @@ void ProgressBar::setVisualProgress(int id, float progress) {
     Drawable* d = mCurrentDrawable;
     if (dynamic_cast<LayerDrawable*>(d)) {
         d = ((LayerDrawable*) d)->findDrawableByLayerId(id);
-        LOGD_IF((d==nullptr)&&(id==R::id::progress),"LayerDrawable lost layer id=%d",id);
+        LOGD_IF((d==nullptr)&&(id==R::id::progress),"LayerDrawable lost layer id=%x",id);
         if (d == nullptr) {
             // If we can't find the requested layer, fall back to setting
             // the level of the entire drawable. This will break if
