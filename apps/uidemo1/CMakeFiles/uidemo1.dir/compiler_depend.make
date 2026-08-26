2ee003d6c2873499ddcf87255513ad0757c8fa59
侯歌 <hou731080@163.com>
2026-08-26 11:15:27 +0800

    :construction:delete uidemo1

--- apps/uidemo1/CMakeFiles/uidemo1.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for uidemo1.
-# This may be replaced when dependencies are built.
--- apps/uidemo1/CMakeFiles/uidemo1_assets.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/uidemo1_assets"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/uidemo1_assets.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
